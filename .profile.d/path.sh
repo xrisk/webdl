a41d0a4a3b7d55063c4c3ceb5119101fa7cb6ddb
Rishav Kundu <rishav.kundu98@gmail.com>
2015-10-18 20:39:45 +0530

    Make .profile.d script path agnostic.

--- .profile.d/path.sh
+++ .profile.d/path.sh
@@ -1,5 +1,5 @@
 export PATH="/app/vendor/bin:"$PATH
 export LD_LIBRARY_PATH="/app/vendor/lib:"$LD_LIBRARY_PATH
 
-mkdir -p "/app/mp3cache"
-mkdir -p "/app/mp4cache"
+mkdir -p "../mp3cache"
+mkdir -p "../mp4cache"
